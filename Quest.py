f03b53d240db904bc545d7f5dc10a67c2ecd545f
ThomasMdt <lazybonehere@gmail.com>
2017-03-06 20:07:51 +0700

    Изменил Quest Сделал user и enemy - словари. У каждого подобного "обьекта" будет свой номер ключей(обязательными для них ключ "name", "xp")

--- Quest.py
+++ Quest.py
@@ -1,13 +1,39 @@
-#Thomas
-#Welcome to Quest
+"""
+Thomas
+Welcome to Quest
+"""
 from tools import quest
-attack = 15
-user_xp = 40
-pahan_xp = 30
-defence = 0
-enemy_xp = [10, 10, 10]
+user = {
+    "name": "",
+    "xp": 40,
+    "attack": 10,
+    "defence": 5
+}
+enemy = {
+    "name": "зэк",
+    "xp": 10,
+    "attack": 10,
+    "defence": 5
+}
 quest("Go ili zassal?", {
-    'GO': lambda: print("VI V HATE! CONGRUTS!"),
-    'else': lambda: exit(0)
+    "GO": lambda: print("VI V HATE! CONGRUTS!"),
+    "else": lambda: exit(0)
 })
-
+print("Вечер в хату! Как звать тебя?\nВведите ваше имя>>")
+user["name"] = input()
+print("Ну здарова,"+user["name"])
+print("Твоя задача выжить в хате!")
+"""
+for_input -> переменная для хранения большего текста для вывода
+используйте ее, если надо вывести большой текст
+"""
+for_input = """
+(1) Ясно, я все понял
+(2) Пошел ты!
+(Вы должны указать цифру или символ, который находиться в скобках)
+"""
+quest(for_input, {
+    "1": lambda: print("Заходи в хату"),
+    "2": lambda: print("Ты получаешь дубинкой по башке"),
+    "else": lambda: print("Ти шо тупой?Я же сказал, что нужно писать то, что в скобках")
+})
--- dice.py
+++ dice.py
@@ -1,12 +1,13 @@
 import random
+
 def play():
-    enemy_sum = [0,0] 
-    user_sum = [0,0]
+    enemy_sum = [0, 0]
+    user_sum = [0, 0]
     print("Зэк предлагает вам сыграть в кости")
     print("Ходит зэк")
     for i in range(2):
         enemy_sum[i] = random.randint(1,6)
-    print("Выпадает {0[0]} и {0[1]}".format(enemy_sum))
+        print("Выпадает {0[0]} и {0[1]}".format(enemy_sum))
     for i in range(2):
         user_sum[i] = random.randint(1,6)
     print("Выпадает {0[0]} и {0[1]}".format(user_sum))
