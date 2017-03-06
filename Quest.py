6395ae10626f80c08a2a25ed0c7428d054ed1a6f
ThomasMdt <lazybonehere@gmail.com>
2017-03-06 23:14:46 +0700

    Добавил две новые функции Изменил API для создания квестов

--- Quest.py
+++ Quest.py
@@ -2,7 +2,7 @@
 Thomas
 Welcome to Quest
 """
-from tools import quest
+from tools import *
 user = {
     "name": "",
     "xp": 40,
@@ -15,8 +15,9 @@ enemy = {
     "attack": 10,
     "defence": 5
 }
+
 quest("Go ili zassal?", {
-    "GO": lambda: print("VI V HATE! CONGRUTS!"),
+    "GO": [lambda: print("VI V HATE! CONGRUTS!")],
     "else": lambda: exit(0)
 })
 print("Вечер в хату! Как звать тебя?\nВведите ваше имя>>")
@@ -33,7 +34,21 @@ for_input = """
 (Вы должны указать цифру или символ, который находиться в скобках)
 """
 quest(for_input, {
-    "1": lambda: print("Заходи в хату"),
-    "2": lambda: print("Ты получаешь дубинкой по башке"),
+    "1": [lambda: print("Заходи в хату")],
+    "2": [lambda: print("Ты получаешь дубинкой по башке"),
+          lambda: change_chartics(user, {"xp": lambda this: this-5})
+          ],
     "else": lambda: print("Ти шо тупой?Я же сказал, что нужно писать то, что в скобках")
+})
+for_input = """"В хате, как известно, всем нужно прописаться. Тебе указывают на койку и говорят, что она твоя. Твои действия?")
+(1) Пойти обустраиваться
+(2) Стоять, как истукан"""
+quest(for_input, {
+    "1": [lambda: print("Тебе нужно было стоять и ждать, куда укажет тебе смотрящий в хате. Ты получаешь -15 к жизням, но за дерзость +5 к атаке"),
+          lambda: change_chartics(user,{"xp": lambda this: this-5, "attack": lambda this: this+3})
+         ],
+    "2": [lambda: print("Ты сделал правильно! Смотрящий через пару секунд указал тебе на место, где обустраиваться. Ты получаешь +5 к атаке и +10 к жизни"),
+          lambda: change_chartics(user, {"xp": lambda this: this+10, "attack": lambda this: this+5})
+         ],
+    "else": [lambda: print("Ти шо тупой?Я же сказал, что нужно писать то, что в скобках")]
 })
--- tools.py
+++ tools.py
@@ -1,11 +1,22 @@
-#Thomas tools module
-#@question -> вопрос, который мы собираемся задавать
-#@option -> варианты ответа, этот параметр является словарем
-def quest(question,option):
+"""
+Thomas tools module
+
+@question -> вопрос, который мы собираемся задавать
+@option -> варианты ответа, этот параметр является словарем
+"""
+
+
+def quest(question, option):
     response = input(question+"\nВаш ответ>")
     for res in option.keys():
         if res.upper() == response.upper():
-            option[res]()
+            for fn in option[res]:
+                fn()
             break
     else:
         option['else']()
+
+
+def change_chartics(obj, param):
+    for key in param.keys():
+        obj[key] = param[key](obj[key])
