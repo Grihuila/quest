9c117cd835d583ddcc64678fda129e6d7a89f251
ThomasMdt <lazybonehere@gmail.com>
2017-03-08 19:45:18 +0700

    Добавил функции: >getting_in_qs >print_available_quest

--- Quest.py
+++ Quest.py
@@ -1,3 +1,7 @@
-from quests import all_quests
-from tools import create_quest
+from constructor import all_quests
+from tools import *
 from Objects import *
+start(user)
+getting_in_qs()
+print_available_quest(all_quests)
+input("[ВАШ ХОД]")
--- constructor.py
+++ constructor.py
@@ -1,10 +1,11 @@
 """
 Thomas module
-Pickle's quests store
+Constructor quests
 """
 from tools import change_chartics
 from Objects import *
 all_quests = [{
+        "title": "Войти в хату",
         "for_input": "(1) Ясно, я все понял\n(2) Пошел ты!\n(Вы должны указать цифру или символ, который находиться в скобках)",
         "1": [lambda: print("Заходи в хату")],
         "2": [lambda: print("Ты получаешь дубинкой по башке"),
