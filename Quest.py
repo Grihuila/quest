f168d28c76b539f1232c7c8c74a0ab9c87308fa5
ThomasMdt <lazybonehere@gmail.com>
2017-03-10 21:37:20 +0700

    Корректрировка диалогов

--- Quest.py
+++ Quest.py
@@ -7,9 +7,9 @@ print_available_quest(all_quests)
 response = input("[ ВАШ ХОД ] >>> ")
 count_quest = len(all_quests)
 if response == "*":
-    print("invent")
+    print("invent") #допилить
 elif response == "#":
-    print("help")
+    print("help") #дополить
 else:
     i_res = int(response)-1
     run_quest(all_quests[i_res])
